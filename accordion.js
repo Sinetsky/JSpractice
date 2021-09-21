9bff950e594377dafd0ffc1c37d1ea6fccc35907
Sinetsky <91119320+Sinetsky@users.noreply.github.com>
2021-09-21 23:51:40 +0300

    Update accordion.js

--- accordion.js
+++ accordion.js
@@ -1,4 +1,19 @@
-const btn = document.querySelector(".feature_link")
-const btns = document.querySelectorAll(".feature_link")
+const btn = document.querySelector(".feature__link")
+const btns = document.querySelectorAll(".feature__link")
+const lists = document.querySelectorAll(".feature-sub")
 
-console.log(btns[0])
+btns.forEach((btnItem, index) => {
+    btnItem.addEventListener('click', () => {
+        btns.forEach((btnItem) => {
+            btnItem.classList.remove("feature__link_active")
+
+        })
+        btnItem.classList.add("feature__link_active")
+
+        lists.forEach((listItem) => {
+            listItem.classList.add("hidden")
+        })
+        lists[index].classList.remove("hidden")
+    })
+})
+
