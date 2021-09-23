f44268e4aa160103e41b50e40c1afbff758b4732
Sinetsky <91119320+Sinetsky@users.noreply.github.com>
2021-09-23 18:01:22 +0300

    Add files via upload

---
+++ modal.js
@@ -0,0 +1,20 @@
+const modalBtn = document.querySelectorAll(".more");
+const modal = document.querySelector(".modal")
+
+modalBtn.forEach(btnItem => {
+    btnItem.addEventListener('click', () => {
+        modal.classList.remove("hidden")
+    })
+
+})
+
+
+modal.addEventListener('click', (event) => {
+    const target = event.target
+
+    if (target.classList.contains("overlay") || target.classList.contains("modal__close")) {
+        modal.classList.add("hidden")
+
+    }
+})
+
